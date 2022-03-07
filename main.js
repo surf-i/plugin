c0181e12b362a9e6af540857f04c12b8a0706938
Juan Alegría <juanszalegria@gmail.com>
2022-03-07 16:28:25 -0500

    Feat: test element insertion while searching

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-if (!window.location.href.match(/https:\/\/www.google.com*\/search\?/)) alert("Bienvenido a Surfi")
+let h = document.getElementById("search");
+
+[].forEach.call(
+  document.querySelectorAll("html div #search h3"),
+  function (el) {
+    let text = el.textContent;
+
+    let stringToColour = function (str) {
+      let hash = 0;
+      for (let i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+      }
+      let colour = "#";
+      for (let i = 0; i < 3; i++) {
+        let value = (hash >> (i * 8)) & 0xff;
+        colour += ("00" + value.toString(16)).substr(-2);
+      }
+      return colour;
+    };
+
+    el.insertAdjacentHTML(
+      "afterend",
+      `
+        <div style="display: inline-flex;">
+        <span style='margin-left:2px;height: 13px;width: 13px; background-color:` +
+        stringToColour(text) +
+        `; border-radius: 50%;'>
+        </span>
+        </div>
+        <p style="margin:0; font-size: 14px">Category: ` +
+        text[0] +
+        ` - 5% level of trust -  Keywords: cafe, surf, mountain, technology</p>
+    `
+    );
+  }
+);
