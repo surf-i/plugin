b4d77659e52d13d64e0eba7d2d8a63a413395316
Juan Romero <juan.romero1201@gmail.com>
2022-03-17 22:23:41 -0500

    Actualizado a surfi.netlify.app

--- onInstall.js
+++ onInstall.js
@@ -1,13 +1,13 @@
 chrome.runtime.onInstalled.addListener(function (details) {
     if (details.reason == "install") {
         chrome.tabs.create({
-            url : "https://www.example.com/install.html"
+            url : "https://surfi.netlify.app"
         });
     }
 
     if (details.reason == "update") {
         chrome.tabs.create({
-            url : "https://www.example.com/update.html"
+            url : "https://surfi.netlify.app"
         });
     }
 });
