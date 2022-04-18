484bc873da5127397af9a65d7ed1ee60ce25a58c
David Sequera <davidsequerads@gmail.com>
2022-04-17 23:59:22 -0500

    Ajustes Categorias

--- main.js
+++ main.js
@@ -1,9 +1,61 @@
 let h = document.getElementById("search");
 
+// fetch();
+
+
+// fetch("https://instagram.fbog4-1.fna.fbcdn.net/v/t51.2885-15/274993184_354708049705462_5251794527509873135_n.jpg?stp=dst-jpg_e35_p1080x1080&_nc_ht=instagram.fbog4-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=Ny_-26Xs8wYAX-hmUIx&tn=rcDQeKrH2J8B4ff_&edm=APU89FABAAAA&ccb=7-4&oh=00_AT8hA0CVXdb_6riKVcn51tty8Txe-ueqS54bPLsPf4zvlQ&oe=62489FD1&_nc_sid=86f79a", {
+//   "referrerPolicy": "strict-origin-when-cross-origin",
+//   "body": null,
+//   "method": "GET",
+//   "mode": "cors",
+//   "credentials": "omit"
+// });
+let query = {
+  "SOCIAL": {
+    "color": "#isidsndis",
+    "symbol": "",
+  },
+  "ENTRETENIMIENTO":{
+    "color": "#isidsndis",
+    "symbol": "",
+  },
+  "NOTICIAS":{
+    "color": "#isidsndis",
+    "symbol": "",
+  },
+  "ACADEMICO":{
+    "color": "#isidsndis",
+    "symbol": "",
+  },
+  "OPINION":{
+    "color": "#isidsndis",
+    "symbol": "",
+  },
+  "COMERCIO":{
+    "color": "#isidsndis",
+    "symbol": "",
+  },
+  "TECNOLOGIA":{
+    "color": "#isidsndis",
+    "symbol": "",
+  },
+  "PRODUCTIVIDAD":{
+    "color": "#isidsndis",
+    "symbol": "",
+  },
+  "TURISMO":{
+    "color": "#isidsndis",
+    "symbol": "",
+  }, 
+
+};
+
+
 [].forEach.call(
   document.querySelectorAll("html div #search h3"),
   function (el) {
     let text = el.textContent;
+    let words = text.split(' ')
 
     let stringToColour = function (str) {
       let hash = 0;
@@ -18,10 +70,12 @@ let h = document.getElementById("search");
       return colour;
     };
 
-    function Keywords(text){
-      let words = text.split(' ')
+    function Keywords(words){
       let number = Math.floor(Math.random() * words.length);
-      return words[number];
+      if(words[number].length > 0)
+        return words[number];
+      else
+        return Keywords(text)
     }
     el.insertAdjacentHTML(
       "afterend",
@@ -32,8 +86,11 @@ let h = document.getElementById("search");
         </span>
         </div>
         <p class="Surfi-text">Category: ` + text[0] +
-        ` - ${Math.floor(Math.random()*100)}% level of trust -  Keywords: ${Keywords(text)},  ${Keywords(text)},  ${Keywords(text)},  ${Keywords(text)}</p>
+        ` - ${Math.floor(Math.random()*100)}% level of trust -  Keywords: ${Keywords(words)},  ${Keywords(words)},  ${Keywords(words)},  ${Keywords(words)}</p>
     `
     );
   }
 );
+
+
+// Busquedas compuestas
