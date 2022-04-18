29f76006e908de7f7f09cd9124b9e4f4aa475500
aq02115 <aq02115@ad.uniandes.edu.co>
2022-04-18 14:27:17 -0500

    popup and mockdb structure start

---
+++ db.js
@@ -0,0 +1,58 @@
+// mock db based in JSON format
+const db = {
+    "TABLE1": [
+        {
+            "name":"registro 1",
+            "atribute": "i could be anything"
+        }
+
+    ],
+    "TABLE2": [
+        {
+            "name":"registro 1 tabla2"
+        }
+    ],
+    "TABLE3":[
+        {
+            "key":"registro 1 tabla3"
+        }
+    ],
+    "CATEGORIES":{
+        "SOCIAL": {
+            "color": "#isidsndis",
+            "symbol": "",
+          },
+          "ENTRETENIMIENTO":{
+            "color": "#isidsndis",
+            "symbol": "",
+          },
+          "NOTICIAS":{
+            "color": "#isidsndis",
+            "symbol": "",
+          },
+          "ACADEMICO":{
+            "color": "#isidsndis",
+            "symbol": "",
+          },
+          "OPINION":{
+            "color": "#isidsndis",
+            "symbol": "",
+          },
+          "COMERCIO":{
+            "color": "#isidsndis",
+            "symbol": "",
+          },
+          "TECNOLOGIA":{
+            "color": "#isidsndis",
+            "symbol": "",
+          },
+          "PRODUCTIVIDAD":{
+            "color": "#isidsndis",
+            "symbol": "",
+          },
+          "TURISMO":{
+            "color": "#isidsndis",
+            "symbol": "",
+          }
+    }
+}
---
+++ pages/logIn.js
@@ -0,0 +1,3 @@
+function Samples(){
+    return null
+}
