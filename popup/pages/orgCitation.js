33302ff1edb9e72621441520e37c1e863bbbbc40
ElReyZero <juan.romero1201@gmail.com>
2022-05-12 09:17:22 -0500

    Added first templates for the citation function

---
+++ popup/pages/orgCitation.js
@@ -0,0 +1,33 @@
+import UserInput from "../components/UserInput.js"
+
+function citateAuthor(object) {
+    return (
+        /*html*/`
+        <div class="SurfiComponent page-container"> 
+            <button id="backButton">
+            <span class="material-icons">arrow_back_ios</span>
+        </button>
+        <h2 class="title">SignUp</h2>
+        <!-- <img src="../../assets/logo/round_logo.png" class="logo" alt="Surfi Logo"> -->
+        <div class="SignInComponent_container" id="citationFormOrg">
+            <form
+            class="SignInComponent_form"
+            >
+            ${UserInput({ id: 'authorNameComp',title: 'Organization Name', type: "text", name:'orgName' })}
+            ${UserInput({ id: 'websiteNameComp',title: 'Website Name', type: "text", name:'websiteName' })}
+            ${UserInput({ id: 'webpageNameComp',title: 'Current Page Name', type: "text", name:'webpagename' })}
+            ${UserInput({ id: 'dateOfPublicationComp',title: 'Date of Publication', type: "date", name:'dateOfPublication' })}    
+            </form>
+            <button 
+                disabled
+                class="sign_in_btn"
+                id="citateButton"
+                type="submit"
+            >
+                Generate Citation
+            </button>
+        </div>
+    </div>
+    `
+    )
+}
---
+++ popup/pages/unknownCitation.js
@@ -0,0 +1,32 @@
+import UserInput from "../components/UserInput.js"
+
+function citateAuthor(object) {
+    return (
+        /*html*/`
+        <div class="SurfiComponent page-container"> 
+            <button id="backButton">
+            <span class="material-icons">arrow_back_ios</span>
+        </button>
+        <h2 class="title">SignUp</h2>
+        <!-- <img src="../../assets/logo/round_logo.png" class="logo" alt="Surfi Logo"> -->
+        <div class="SignInComponent_container" id="citationFormUnknown">
+            <form
+            class="SignInComponent_form"
+            >
+            ${UserInput({ id: 'websiteNameComp',title: 'Website Name', type: "text", name:'websiteName' })}
+            ${UserInput({ id: 'webpageNameComp',title: 'Current Page Name', type: "text", name:'webpagename' })}
+            ${UserInput({ id: 'dateOfPublicationComp',title: 'Date of Publication', type: "date", name:'dateOfPublication' })}    
+            </form>
+            <button 
+                disabled
+                class="sign_in_btn"
+                id="citateButton"
+                type="submit"
+            >
+                Generate Citation
+            </button>
+        </div>
+    </div>
+    `
+    )
+}
