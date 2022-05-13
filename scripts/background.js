33db24df036b52d3d88578062ecd4495424f688c
Your Name <you@example.com>
2022-05-13 00:12:48 -0500

    Surfi Search Completed (Surfen Version)

--- scripts/background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onInstalled.addListener(function (details) {
-    if (details.reason === "install") {
-        chrome.tabs.create({
-            url : "https://surfi.netlify.app"
-        });
-    }
-});
-
