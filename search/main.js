c03bf94f9439975e34c04e68a30b0715b0e44a02
Your Name <you@example.com>
2022-05-13 00:22:56 -0500

    SurfiSearch Completed

--- search/main.js
+++ search/main.js
@@ -1,79 +1,99 @@
-// Edits 
-// [].forEach.call(
-//   browserDocument = document.querySelectorAll("html div #search .DKV0Md"),
-//   function (el) {
-//     let text = el.textContent;
-//     let words = text.split(' ')
-
-//     let stringToColour = function (str) {
-//       let hash = 0;
-//       for (let i = 0; i < str.length; i++) {
-//         hash = str.charCodeAt(i) + ((hash << 5) - hash);
-//       }
-//       let colour = "#";
-//       for (let i = 0; i < 3; i++) {
-//         let value = (hash >> (i * 8)) & 0xff;
-//         colour += ("00" + value.toString(16)).substr(-2);
-//       }
-//       return colour;
-//     };
-
-//     function Keywords(words){
-//       let number = Math.floor(Math.random() * words.length);
-//       if(words[number].length > 1)
-//         return words[number];
-//       else
-//         return Keywords(words)
-//     }
-//     el.insertAdjacentHTML(
-//       "afterend",
-//       `
-//         <div style="display: inline-flex;">
-//         <span class="Dashboard-ball">
-//           <p>${text[0]}</p>
-//         </span>
-//         </div>
-//         <p class="Surfi-text">Category: ` + text[0] +
-//         ` - ${Math.floor(Math.random()*100)}% level of trust -  Keywords: ${Keywords(words)},  ${Keywords(words)},  ${Keywords(words)},  ${Keywords(words)}</p>
-//     `
-//     );
-//   }
-// );
 let surfiSearch = (element) =>{
-  return `
+  return /*html*/`
   <div class="surfiSearch">
-  <div class="searchCategory ${element.category}">
-  <p class="searchCategoryTitle">${element.category}</p>
-  </div>
-  <div class="searchDataContainer">
-  <div class="veracity">
-  <p class="searchData">${Math.floor(Math.random()*30)+70}%</p>
-  </div>
-  <div class="calification">
-  <p class="searchData">⭐${(Math.floor(Math.random()*20)+30)/10}</p>
-  </div>
-  </div>
-  <div class="AuthorandDate">
-  <p class="searchParagraph date">Fecha:${Math.floor(Math.random()*30)}/${Math.floor(Math.random()*12)}/22</p>
-  <p class="searchParagraph author">Author: N/A</p>
-  </div>
+    <div class="searchCategory ${element.category}">
+      <p class="searchCategoryTitle">${element.category}</p>
+    </div>
+    <div class="searchDataContainer">
+    <div class="veracity">
+    <p class="searchData">${element.veracity}%</p>
+    </div>
+    <div class="calification">
+    <p class="searchData">⭐${element.rating}</p>
+    </div>
+    </div>
+    <div class="AuthorandDate">
+    <p class="searchParagraph date">Fecha:${element.date}/22</p>
+    <p class="searchParagraph author">Author: ${element.author}</p>
+    </div>
   </div>
   `
 }
-// let categories = ['Herramienta','Social','Entretenimiento','Periodismo','Investigacion','Empresas','Tienda'];
-// let Pariscat = ['Investigacion','Periodismo','Entretenimiento','Tienda','Herramienta','Social','Empresas'];
-let Pariscat = ['Investigacion','Organizacion','Comercio','Periodismo','Entretenimiento','Organizacion','Social'];
-let counter = 0;
-[].forEach.call(
-  browserDocument = document.querySelectorAll("html div #search .g"),
-  function (element) {
-    let category = Pariscat[counter%Pariscat.length]
-    let text = element.textContent;
-    console.log(text)
-    element.insertAdjacentHTML("afterbegin",surfiSearch({category: category}));
-    counter++
+// let categoriesv1 = ['Herramienta','Social','Entretenimiento','Periodismo','Investigacion','Empresas','Tienda'];
+// let categoriesv2 = ['Investigacion','Periodismo','Entretenimiento','Tienda','Herramienta','Social','Empresas'];
+class surfiAddon
+{
+  constructor(category,rating,author,date,veracity)
+  {
+    this.category = category;
+    this.rating = rating;
+    this.author = author;
+    this.date = date;
+    this.veracity = veracity;
   }
-);
+}
+
+var linksElements = new Map();
+let webSearchs = document.querySelectorAll("html div #search .g");
+for(element of webSearchs)
+{
+    let link = element.querySelector('a')
+    let linkVar = document.createElement('a')
+    linkVar.href = link.href
+    linksElements.set("https://"+linkVar.hostname,element);
+};
+
+
+//Send requests with the array of links
+async function getMultipleWebsites(links){
+     let surfiRequest = new Map();
+     let url = "https://44.195.183.116"
+     let string =""
+     let contador = true;
+     for (let link of links) 
+     {
+       if (contador)
+       {
+         contador = false;
+         var tmp = document.createElement('a')
+         tmp.href = link
+         string+="?url=https://"+tmp.hostname;
+       }
+        else
+        {
+        string += "&url="+link
+        }
+     }
+     const response = await fetch(url+'/websites/multiple/'+string);
+     let res = await response.json()
+     for (let element of res)
+     {
+      surfiRequest.set(element.url,new surfiAddon(element.categoria,Math.round(element.calificacionPromedio*10)/10,element.autor,element.fecha,Math.round(element.gradoVeracidadPromedio)));
+     }
+     return surfiRequest;
+} 
+
+
+
+async function changePage()
+{
+  var surfiReq = await getMultipleWebsites(linksElements.keys());
+  for(let link of linksElements.keys())
+  { 
+  //console.log("probando surfiReq"+surfiReq.get(link).category);
+  let element = linksElements.get(link);
+  if (surfiReq.has(link))
+  {
+    element.insertAdjacentHTML("afterbegin",surfiSearch(surfiReq.get(link)));
+  }
+  else
+  {
+    element.insertAdjacentHTML("afterbegin",surfiSearch(new surfiAddon('No calificado','NA','NA','NA','NA')));
+  }
+  }
+}
+
+changePage()
 
 
 // Busquedas compuestas
