ee20e9ffd961d40b763efb986bdf98ee2307e348
Juan Alegría <juanszalegria@gmail.com>
2022-05-14 20:10:55 -0500

    Fix: load page info on section change

--- popup/main/popup.js
+++ popup/main/popup.js
@@ -41,79 +41,79 @@ setPage(state)
 var backButton = document.getElementById("backButton")?.addEventListener("click", function () { setPage('start') });
 
 function setPage(page) {
-    deleteElement(body.children[0])
-    Cookies.set('before',(before = before ==  state? before: state))
-    Cookies.set('state',(state = page))
-    console.log(state)
-    addElement(null, routes[state], body)
+  deleteElement(body.children[0])
+  Cookies.set('before', (before = before == state ? before : state))
+  Cookies.set('state', (state = page))
+  console.log(state)
+  addElement(null, routes[state], body)
 
-    
-    if (state === 'start') {
-        var event = document.getElementById("StartToLogInButton").addEventListener("click", function () { setPage('login') });
-        var SignUpButton = document.getElementById("LogInToSignUpButton").addEventListener("click", function () { setPage('signup') });
-        // fetch(url+'websites/?url=www.gooogle.com')
-        // .then(response => response.json())
-        // .then(data => console.log(data));
-    }
-    if(state === 'signup'){
-      let email = document.getElementById('email')
-      let username = document.getElementById('username')
-      let password = document.getElementById('password')
-      let password2 = document.getElementById('passwordVerif')
-      
-      //eventos feos
-      let emailEvent = email.addEventListener('input', function (){validateForm(email, username,password,password2)})
-      let usernameEvent = username.addEventListener('input', function (){validateForm(email, username,password,password2)})
-      let passwordEvent = password.addEventListener('input', function (){validateForm(email, username,password,password2)})
-      let password2Event = password2.addEventListener('input', function (){validateForm(email, username,password,password2)})
-  
-      var SignUpButton = document.getElementById("SignUpToHomeButton").addEventListener("click", function () { signUp()});
-    }
-    if (state === 'login') {
-      var eventLog = document.getElementById("LogInToStartButton").addEventListener("click", function (event) {logIn(event) });   
-      var eventLog = document.getElementById("LogInToSignUpButton").addEventListener("click", function (event) {setPage('signup') });   
-      /*
-        try{
-            logIn("surfitest@yopmail.com","","testpass123")
-        }catch{
-            //  
-        }
-        */
-    }
-    if(state === 'login' || state === 'signup' || state === 'review' || state == 'pageinfo' ){
-      var backButton = document.getElementById("backButton").addEventListener("click", function () { setPage(before) });
-    }
-    if (state === 'review') {
-      let category = document.getElementById('optionMenu')
-      let star1 = document.getElementById('star1')
-      let star2 = document.getElementById('star2')
-      let star3 = document.getElementById('star3')
-      let star4 = document.getElementById('star4')
-      let star5 = document.getElementById('star5')
 
-      let categoryEvent = category.addEventListener('change', function (){validateCategory(category)})
-      let star1Event = star1.addEventListener('click', function (){updateRating(star1)})
-      let star2Event = star2.addEventListener('click', function (){updateRating(star2)})
-      let star3Event = star3.addEventListener('click', function (){updateRating(star3)})
-      let star4Event = star4.addEventListener('click', function (){updateRating(star4)})
-      let star5Event = star5.addEventListener('click', function (){updateRating(star5)})
+  if (state === 'start') {
+    var event = document.getElementById("StartToLogInButton").addEventListener("click", function () { setPage('login') });
+    var SignUpButton = document.getElementById("LogInToSignUpButton").addEventListener("click", function () { setPage('signup') });
+    // fetch(url+'websites/?url=www.gooogle.com')
+    // .then(response => response.json())
+    // .then(data => console.log(data));
+  }
+  if (state === 'signup') {
+    let email = document.getElementById('email')
+    let username = document.getElementById('username')
+    let password = document.getElementById('password')
+    let password2 = document.getElementById('passwordVerif')
 
-      var eventReview = document.getElementById("ReviewToStartButton").addEventListener("click", function (event) { review(event) });
-    }
-    if(state === 'account' || state === 'home' || state === 'settings'){
-      var menuHomeButton = document.getElementById("menuHomeButton").addEventListener("click", function () { setPage('home') });
-      var menuAccountButton = document.getElementById("menuAccountButton").addEventListener("click", function () { setPage('account') });
-      var menuSetttingsButton = document.getElementById("menuSetttingsButton").addEventListener("click", function () { setPage('settings') });
-      
-    }
-    if (state === 'home'){
-      var eventHomeReview = document.getElementById("HomeToReviewButton").addEventListener("click", function () { setPage('review') });
-      var eventHomeReview = document.getElementById("HomeToPageInfoButton").addEventListener("click", function () { setPage('pageinfo') });
-      
-    }
-    if(state === 'settings'){
-      var LogOut = document.getElementById("logOutButton").addEventListener("click", function () { setPage('start') });
-    }
+    //eventos feos
+    let emailEvent = email.addEventListener('input', function () { validateForm(email, username, password, password2) })
+    let usernameEvent = username.addEventListener('input', function () { validateForm(email, username, password, password2) })
+    let passwordEvent = password.addEventListener('input', function () { validateForm(email, username, password, password2) })
+    let password2Event = password2.addEventListener('input', function () { validateForm(email, username, password, password2) })
+
+    var SignUpButton = document.getElementById("SignUpToHomeButton").addEventListener("click", function () { signUp() });
+  }
+  if (state === 'login') {
+    var eventLog = document.getElementById("LogInToStartButton").addEventListener("click", function (event) { logIn(event) });
+    var eventLog = document.getElementById("LogInToSignUpButton").addEventListener("click", function (event) { setPage('signup') });
+    /*
+      try{
+          logIn("surfitest@yopmail.com","","testpass123")
+      }catch{
+          //  
+      }
+      */
+  }
+  if (state === 'login' || state === 'signup' || state === 'review' || state == 'pageinfo') {
+    var backButton = document.getElementById("backButton").addEventListener("click", function () { setPage(before) });
+  }
+  if (state === 'review') {
+    let category = document.getElementById('optionMenu')
+    let star1 = document.getElementById('star1')
+    let star2 = document.getElementById('star2')
+    let star3 = document.getElementById('star3')
+    let star4 = document.getElementById('star4')
+    let star5 = document.getElementById('star5')
+
+    let categoryEvent = category.addEventListener('change', function () { validateCategory(category) })
+    let star1Event = star1.addEventListener('click', function () { updateRating(star1) })
+    let star2Event = star2.addEventListener('click', function () { updateRating(star2) })
+    let star3Event = star3.addEventListener('click', function () { updateRating(star3) })
+    let star4Event = star4.addEventListener('click', function () { updateRating(star4) })
+    let star5Event = star5.addEventListener('click', function () { updateRating(star5) })
+
+    var eventReview = document.getElementById("ReviewToStartButton").addEventListener("click", function (event) { review(event) });
+  }
+  if (state === 'account' || state === 'home' || state === 'settings') {
+    var menuHomeButton = document.getElementById("menuHomeButton").addEventListener("click", function () { setPage('home') });
+    var menuAccountButton = document.getElementById("menuAccountButton").addEventListener("click", function () { setPage('account') });
+    var menuSetttingsButton = document.getElementById("menuSetttingsButton").addEventListener("click", function () { setPage('settings') });
+
+  }
+  if (state === 'home') {
+    var eventHomeReview = document.getElementById("HomeToReviewButton").addEventListener("click", function () { setPage('review') });
+    var eventHomeReview = document.getElementById("HomeToPageInfoButton").addEventListener("click", function () { setPage('pageinfo'); loadPageInfo() });
+
+  }
+  if (state === 'settings') {
+    var LogOut = document.getElementById("logOutButton").addEventListener("click", function () { setPage('start') });
+  }
 
 
 }
@@ -138,13 +138,14 @@ async function signUp(event) {
   }
 }
 
-async function review(event){
+async function review(event) {
   let logInToken = Cookies.get('token')
 
-  if(logInToken != null){
+  if (logInToken != null) {
     reviewFunction(event)
     setPage('pageinfo')
-  }else{
+    loadPageInfo()
+  } else {
     console.error("error need login")
     setPage('login')
   }
