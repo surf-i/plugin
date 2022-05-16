22e48a39cff97fa7028cce789cafa9f75e051c0a
Juan Romero <juan.romero1201@gmail.com>
2022-05-15 20:49:28 -0500

    Agregado citation results

---
+++ popup/pages/citationResults.js
@@ -0,0 +1,32 @@
+function citationResultsTemplate(object) {
+    return (
+        /*html*/`
+        <div class="SurfiComponent page-container">
+        <link rel="stylesheet" href="../main/popup.css">
+        <button id="backButton">
+            <span class="material-icons">arrow_back_ios</span>
+        </button>
+        <div class="summary">
+            <h3 class="summary_title">APA</h3>
+            <p class="summary_text" id="apaCitation"></p>
+        </div>
+        <div class="summary">
+        <h3 class="summary_title">IEEE</h3>
+        <p class="summary_text" id="ieeeCitation"></p>
+
+        <div class="summary">
+        <h3 class="summary_title">Chicago</h3>
+        <p class="summary_text" id="chicagoCitation"></p>
+    </div>
+    </div>
+    </div>
+    `
+    )
+}
+
+function applyCitations(apa, ieee, chicago)
+{
+    document.getElementById("apaCitation").innerHTML = apa;
+    document.getElementById("ieeeCitation").innerHTML = ieee;
+    document.getElementById("chicagoCitation").innerHTML = chicago;
+}
